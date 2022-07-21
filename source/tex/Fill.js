3c26c9e99eb5720cf2078426c6c1806c1e6ad799
Jakub21 <jakubp2101@gmail.com>
2022-07-21 13:40:51 +0200

    Update Fill.js

--- source/tex/Fill.js
+++ source/tex/Fill.js
@@ -7,12 +7,6 @@ module.exports = class Fill extends Component {
   }
   draw(ctx, path) {
     this.style.apply(this.constructor.name, ctx);
-    let fs_path = new Path2D();
-    fs_path.moveTo(-1920, -1080);
-    fs_path.lineTo(1920, -1080);
-    fs_path.lineTo(1920, 1080);
-    fs_path.lineTo(-1920, 1080);
-    fs_path.closePath();
-    ctx.fill(fs_path);
+    ctx.fill(path);
   }
 }
