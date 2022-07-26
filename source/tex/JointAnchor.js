22835bd3430d320752e13dbd23d52ab9ca7da3ba
Jakub21 <jakubp2101@gmail.com>
2022-07-26 13:49:48 +0200

    Update JointAnchor texture component

--- source/tex/JointAnchor.js
+++ source/tex/JointAnchor.js
@@ -10,17 +10,18 @@ module.exports = class JointAnchor extends Component {
   }
   draw(ctx) {
     // NOTE: Styles are ignored
-    // NOTE: Assumes no transform of the element itself, only joint
+    // NOTE: Assumes no only transform other than scale of the limb itself
     let joint = this.elm.trf.parent.elm;
     if (joint == undefined) return;
     if (joint.constructor.name != 'Joint') return;
-    let v = joint.current._offset;
-    let point = new Vector(-v.x, -v.y);
+    let v = joint.current._offset.copy();
+    let radius = 8 / this.elm.trf._scale;
+    v.mult(-1/this.elm.trf._scale);
     let path = new Path2D();
-    path.arc(...point.get(), this.radius, 0, 2*Math.PI);
+    path.arc(...v.get(), radius, 0, 2*Math.PI);
     ctx.fillStyle = '#090';
     ctx.strokeStyle = '#3F3';
-    ctx.lineWidth = this.radius;
+    ctx.lineWidth = radius;
     ctx.stroke(path);
     ctx.fill(path);
   }
