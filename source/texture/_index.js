5689b7a3f6675b4bac923287d406ba96299c6335
Jakub21 <jakubp2101@gmail.com>
2023-05-01 20:20:43 +0200

    Ensure imports in index files are sorted

--- source/texture/_index.js
+++ source/texture/_index.js
@@ -1,7 +1,7 @@
 
-export {default as Texture} from './Texture.js';
-export {default as Component} from './Component.js';
-
 export * as components from './components/_index.js';
 export * as styles from './styles/_index.js';
 export * as utility from './utility/_index.js';
+
+export {default as Component} from './Component.js';
+export {default as Texture} from './Texture.js';
--- source/texture/utility/_index.js
+++ source/texture/utility/_index.js
@@ -1,5 +1,5 @@
 
-export {default as Triangles} from './Triangles.js';
-export {default as JointAnchor} from './JointAnchor.js';
 export {default as BoundingBox} from './BoundingBox.js';
 export {default as Center} from './Center.js';
+export {default as JointAnchor} from './JointAnchor.js';
+export {default as Triangles} from './Triangles.js';
